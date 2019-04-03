ccde2740dd489fff0425a3c5a650a89780964da6
xiaohui <1002714042@qq.com>
2019-04-03 10:42:08 +0800

    Add files via upload

---
+++ stair.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+//每次可与走一阶或者两阶
+
+int count = 0;
+
+void main()
+{
+	stair(100,1);
+	stair(100,2);
+	
+	prinf("each of the possible Numbers is %d\n",count);
+
+	return ;
+}
+
+int stair(int nums, int sub_nums)
+{
+	
+	//static int count = 0;
+	int sub;
+
+	sub = nums-sub_nums;
+	if(sub == 0){
+		count++;
+		return 0;
+	}else if(sub < 0){
+		return -1;
+	}
+	stair(sub, 1);
+	stair(sub, 2);
+	
+	return 0;
+}
